2a9e7fa21ad7fa22d57f77854cd0a4dd6a89b22c
Michelle Yue Wang <yue.7.wang@kcl.ac.uk>
2022-10-02 20:06:12 +0100

    Done

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+def isPrime(num):
+    if num <= 1:
+        return False
+    for x in range(2, num):
+        if num % x == 0:
+            return False
+    return True
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f'number of primes: {number_of_primes} must be larger than 0')
     list = []
+    cnt = 0
+    num = 2
+    while cnt < number_of_primes:
+        if isPrime(num):
+            cnt += 1
+            list.append(num)
+        num += 1
     return list
